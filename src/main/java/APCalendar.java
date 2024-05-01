9622f3e4e510353e99142903a08d3981bdc5ef76
LucaTieman2 <143014925+LucaTieman2@users.noreply.github.com>
2024-05-01 09:40:09 -0700

    Update APCalendar.java

--- src/main/java/APCalendar.java
+++ src/main/java/APCalendar.java
@@ -12,7 +12,13 @@ public class APCalendar
    */
   public static int numberOfLeapYears(int year1, int year2)
   { 
-    /* to be implemented in part (a) */
+    int count = 0;
+    for (int i = year1; y <= year2; y++) {
+      if (isLeapYear(y)) {
+        count++;
+      }
+    }
+    return count;
 
   }
   
